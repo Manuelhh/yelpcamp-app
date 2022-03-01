7abd647c3d4dda58322efbe34f63d88f8c0b8d41
Manuel Hinojosa <mangud.hinojosa@gmail.com>
2022-03-01 11:02:26 -0500

    Added full CRUD

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -19,8 +19,38 @@ const getNewCampgroundForm = (req, res, next) => {
   res.render("campgrounds/new-campground-form");
 };
 
+const createACampground = async (req, res, next) => {
+  const newCampground = await new Campground.Campground(req.body);
+  await newCampground.save();
+  res.redirect(`/campgrounds/${newCampground._id}`);
+};
+
+const editOneCampgroundForm = async (req, res, next) => {
+  const campgroundToEdit = await Campground.Campground.findById(req.params.id);
+  res.render("campgrounds/edit-campground-form", { campgroundToEdit });
+};
+
+const editOneCampground = async (req, res, next) => {
+  const { id } = req.params;
+  const campgroundToEdit = await Campground.Campground.findByIdAndUpdate(
+    id,
+    req.body
+  );
+  res.redirect(`/campgrounds/${id}`);
+};
+
+const deleteOneCampground = async (req, res, next) => {
+  const { id } = req.params;
+  const campgroundToDelete = await Campground.Campground.findByIdAndDelete(id);
+  res.redirect("/campgrounds");
+};
+
 module.exports = {
   getAllCampgrounds,
   getOneCampground,
   getNewCampgroundForm,
+  createACampground,
+  editOneCampgroundForm,
+  editOneCampground,
+  deleteOneCampground,
 };
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,6 +4,10 @@ const campgroundsControllers = require("../controllers/campgrounds");
 
 router.get("/", campgroundsControllers.getAllCampgrounds);
 router.get("/new", campgroundsControllers.getNewCampgroundForm);
+router.post("/", campgroundsControllers.createACampground);
 router.get("/:id", campgroundsControllers.getOneCampground);
+router.get("/:id/edit", campgroundsControllers.editOneCampgroundForm);
+router.put("/:id/", campgroundsControllers.editOneCampground);
+router.delete("/:id", campgroundsControllers.deleteOneCampground);
 
 module.exports = router;
