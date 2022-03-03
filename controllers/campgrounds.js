88647a385658b595289d6570cda94c8f33bb34a6
Manuel Hinojosa <mangud.hinojosa@gmail.com>
2022-03-03 08:33:07 -0500

    Started server side error handleing

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,49 +1,50 @@
 const Campground = require("../models/campground");
+const catchAsync = require("./utils/catchAsync");
 
-const getAllCampgrounds = async (req, res, next) => {
+const getAllCampgrounds = catchAsync(async (req, res, next) => {
   const allCampgrounds = await Campground.Campground.find({});
   res.render("campgrounds/all-campgrounds", {
     allCampgrounds,
   });
-};
+});
 
-const getOneCampground = async (req, res, next) => {
+const getOneCampground = catchAsync(async (req, res, next) => {
   const { id } = req.params;
   const oneCampground = await Campground.Campground.findOne({ _id: id });
   res.render("campgrounds/one-campground", {
     oneCampground,
   });
-};
+});
 
-const getNewCampgroundForm = (req, res, next) => {
+const getNewCampgroundForm = catchAsync((req, res, next) => {
   res.render("campgrounds/new-campground-form");
-};
+});
 
-const createACampground = async (req, res, next) => {
+const createACampground = catchAsync(async (req, res, next) => {
   const newCampground = await new Campground.Campground(req.body);
   await newCampground.save();
   res.redirect(`/campgrounds/${newCampground._id}`);
-};
+});
 
-const editOneCampgroundForm = async (req, res, next) => {
+const editOneCampgroundForm = catchAsync(async (req, res, next) => {
   const campgroundToEdit = await Campground.Campground.findById(req.params.id);
   res.render("campgrounds/edit-campground-form", { campgroundToEdit });
-};
+});
 
-const editOneCampground = async (req, res, next) => {
+const editOneCampground = catchAsync(async (req, res, next) => {
   const { id } = req.params;
   const campgroundToEdit = await Campground.Campground.findByIdAndUpdate(
     id,
     req.body
   );
   res.redirect(`/campgrounds/${id}`);
-};
+});
 
-const deleteOneCampground = async (req, res, next) => {
+const deleteOneCampground = catchAsync(async (req, res, next) => {
   const { id } = req.params;
   const campgroundToDelete = await Campground.Campground.findByIdAndDelete(id);
   res.redirect("/campgrounds");
-};
+});
 
 module.exports = {
   getAllCampgrounds,
